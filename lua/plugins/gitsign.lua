4a9ddbedaaa2af24811689a25b89f2790fa82426
thanh182790 <nguyenvanthanh9876543210@gmail.com>
2025-08-24 23:28:50 +0700

    Fix gitsign plugin

--- lua/plugins/gitsign.lua
+++ lua/plugins/gitsign.lua
@@ -1,12 +1,19 @@
 return {
   'lewis6991/gitsigns.nvim',
-  event = { "BufReadPre", "BufNewFile" },
   opts = {
+    -- See `:help gitsigns.txt`
     signs = {
-      add          = { text = '+' },
-      change       = { text = '~' },
-      delete       = { text = '_' },
-      topdelete    = { text = '‾' },
+      add = { text = '+' },
+      change = { text = '~' },
+      delete = { text = '_' },
+      topdelete = { text = '‾' },
+      changedelete = { text = '~' },
+    },
+    signs_staged = {
+      add = { text = '+' },
+      change = { text = '~' },
+      delete = { text = '_' },
+      topdelete = { text = '‾' },
       changedelete = { text = '~' },
     },
   },
